7410706d95179868ac50c05424e7ae1c36555a2e
patel-jai <78267459+patel-jai@users.noreply.github.com>
2024-04-05 12:12:59 -0600

    Update test.php

--- test/test.php
+++ test/test.php
@@ -5,8 +5,11 @@ use PHPUnit\Framework\TestCase;
 class test extends TestCase
 {
     public function testSomething()
-    {
-        $result = "Hello";
-        $this->assertEquals("Hello", $result);
+    {     
+        ob_start();
+        include 'homepage.php';
+        $output = ob_get_clean();
+        $this->assertStringContainsString('<title>Hotel Booking Management System</title>', $output);
     }
 }
+
